62e67c4b6c1645c5bf05749c84d9559564b9e215
woong-sung <cjswo138@gmail.com>
2023-01-02 19:19:03 +0900

    feat: 회원가입 메서드 작성

--- song/src/main/java/jojo/solo/song/member/MemberController.java
+++ song/src/main/java/jojo/solo/song/member/MemberController.java
@@ -1,12 +1,21 @@
 package jojo.solo.song.member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberDto.Response signIn(@RequestBody MemberDto.SignIn signIn) {
+        Member member = memberService.save(signIn);
+
+        return memberMapper.memberToMemberResponse(member);
+    }
 }
